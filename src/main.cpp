2d30cf450a05f515b062d1e03a160e8d803e9d94
root <bluebirid228@gmail.com>
2020-06-05 15:57:43 +0000

    add mandelbrot generation

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,32 @@ using namespace std;
 float width =  600;
 float height = 600;
 
+
+// Necessary Functions
+int value (int x, int y)  {
+  // we divide by the image dimensions to get values smaller than 1
+  complex<float> point( (float)x/width-1.5, (float)y/height-0.5);
+  // then apply a translation
+  complex<float> z(0, 0);
+      unsigned int nb_iter = 0;
+      while (abs (z) < 2 && nb_iter <= 34) {
+             z = z * z + point;
+             nb_iter++;
+      }
+      if (nb_iter < 34) return 255;
+      else return 0;
+}
+
+
 // Main Function
 int main ()  {
     ofstream my_Image ("mandelbrot.ppm");
-    if (my_Image.is_open ()) {
+    if (my_Image.is_open()) {
         my_Image << "P3\n" << width << " " << height << " 255\n";
         for (int i = 0; i < width; i++) {
              for (int j = 0; j < height; j++)  {
-                  my_Image << 255<< ' ' << 0 << ' ' << 0 << "\n";
+               	 int val = value(i, j); 
+                 my_Image << val << ' ' << 0 << ' ' << 0 << "\n";
              }
         }
         my_Image.close();
