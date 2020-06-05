85e752eaa94877063a1ebc76acaf9303caa1cab7
root <bluebirid228@gmail.com>
2020-06-05 15:45:54 +0000

    build .ppm file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream> // for files manipulation
+#include <complex> // for complex numbers
+using namespace std;
+ 
+// Variables
+float width =  600;
+float height = 600;
 
-int main() {
-    std::cout << "Hello World!" << "\n";
+// Main Function
+int main ()  {
+    ofstream my_Image ("mandelbrot.ppm");
+    if (my_Image.is_open ()) {
+        my_Image << "P3\n" << width << " " << height << " 255\n";
+        for (int i = 0; i < width; i++) {
+             for (int j = 0; j < height; j++)  {
+                  my_Image << 255<< ' ' << 0 << ' ' << 0 << "\n";
+             }
+        }
+        my_Image.close();
+    }
+    else cout << "Could not open the file";
     return 0;
 }
